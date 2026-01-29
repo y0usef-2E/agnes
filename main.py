60175b0c83536d6713802130679c1d2655e40211
y0usef-2E <y0usef-2e@mailbox.org>
2026-01-29 17:52:11 +0100

    tweak frontend

--- main.py
+++ main.py
@@ -8,34 +8,36 @@ import subprocess
 # change this, if you are on windows:
 VISUAL_STUDIO_AT = R"C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat"
 
-
 exec = "agnes" 
 if os.name == "nt":
     exec = exec + ".exe"
 
-def build():
+def build(with_build_log: bool):
     if not os.path.exists("build"):
         os.mkdir("build")
     
     source_file = os.path.abspath("parser.c")
     
     if os.name == "nt": 
-        subprocess.run(["build.bat", VISUAL_STUDIO_AT, source_file])
+        subprocess.run(["build.bat", VISUAL_STUDIO_AT, source_file], capture_output=(not with_build_log))
     else:
         os.chdir("build")
         subprocess.run(["gcc", source_file, "-o", "{exec}"])
         os.chdir("..")        
-build()
 
 argparser = argparse.ArgumentParser(
     prog="agnes", description="JSON parser."
 )
 
 argparser.add_argument("file")
+argparser.add_argument('--build-log', action=argparse.BooleanOptionalAction)
 
 args = argparser.parse_args()
 
 file_path = args.file
+with_build_log = args.build_log
+
+build(with_build_log)
 
 if not os.path.exists(file_path):
     print("[IO Error] file not found.")
